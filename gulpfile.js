840d48db5e2a9230ef93765cfa53c1c09c453964
Josh Barr <josh@springload.co.nz>
2014-08-31 12:24:16 +1200

    responsive styles start

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,25 @@
 var gulp = require("gulp");
 
 
-gulp.task('sass', function (cb) {
+gulp.task('sass', function () {
     var sass = require("gulp-sass");
-
-    gulp.src("./src/sass/**/*.scss")
-        .pipe(sass({sourceComments: 'none'}))
-        .pipe(gulp.dest("./www/assets"))
-
-    cb();
-});
-
-
-gulp.task('autoprefixer', function () {
     var postcss      = require('gulp-postcss');
     var sourcemaps   = require('gulp-sourcemaps');
     var autoprefixer = require('autoprefixer-core');
+    var processors = [
+        autoprefixer({ browsers: ['> 1%', 'IE 9'] })
+    ];
 
-    return gulp.src('./www/assets/*.css')
+    gulp.src("./src/sass/**/*.scss")
+        .pipe(sass({sourceComments: 'none'}))
         .pipe(sourcemaps.init())
-        .pipe(postcss( [autoprefixer] ))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./www/assets'));
+            .pipe(postcss( processors ))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('./www/assets'));
+
 });
 
-
-gulp.task("css", ["sass", "autoprefixer"]);
+gulp.task("css", ["sass"]);
 
 gulp.task("watch", function() {
     gulp.watch("./src/sass/**/*.scss", ["sass"]);
